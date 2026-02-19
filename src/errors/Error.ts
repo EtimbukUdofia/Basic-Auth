cda0f344d8556b1d7fdf808717bdc6544fc67f33
EtimbukUdofia <etimbukudofia9@gmail.com>
2026-02-19 14:41:23 +0100

    feat: add custom errors with additional types for errors and Express namespace
    Add optional 'user' object to Express namespace

---
+++ src/errors/Error.ts
@@ -0,0 +1,49 @@
+export class CustomError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class BadRequestError extends CustomError {
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends CustomError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenError extends CustomError {
+  constructor(message = "Forbidden") {
+    super(message, 403);
+  }
+}
+
+export class NotFoundError extends CustomError {
+  constructor(message = "Not Found") {
+    super(message, 404);
+  }
+}
+
+export class ConflictError extends CustomError {
+  constructor(message = "Conflict") {
+    super(message, 409);
+  }
+}
+
+export class ServerError extends CustomError {
+  constructor(message = "Server Error") {
+    super(message, 500);
+  }
+}
+
+export class ServiceUnavailableError extends CustomError {
+  constructor(message = "Service Unavailable") {
+    super(message, 503);
+  }
+}
